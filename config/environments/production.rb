cc33ee109b228d0512dea51361b1f00164308dc4
Matías Vergara <mvergarair@gmail.com>
2016-01-31 10:35:38 -0300

    mail fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,10 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_mailer.default_url_options = {:host => "mvergarair.cl"}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
